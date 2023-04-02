74ada758f2e45bb0f30270103c06b929e31fae46
Nimesh Korat <nimeshkorat611@gmail.com>
2023-04-02 13:06:45 +0530

    Initial commit

---
+++ lib/asteroid_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+
+class AsteroidData {
+  final Size size;
+  Alignment alignment;
+  final String path = "assets/asteroid.png";
+
+  AsteroidData({required this.alignment, required this.size});
+
+
+}
